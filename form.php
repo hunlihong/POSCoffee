e4b6e7df498e39d163536bc05a0d287d6f6bd0e5
Phan ChhayLeap <phanchhayleap.scientist@gmail.com>
2024-02-28 14:50:44 +0700

    .

--- form.php
+++ form.php
@@ -1,43 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" type="text/css" href="form.css" />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
-    />
-    <title>Document</title>
-  </head>
-  <body>
-    <div class="formcon">
-      <form action="" class="form_main">
-        <p class="heading"><img src="image/coffe.png" /></p>
-        <div class="inputContainer">
-          <span class="material-symbols-outlined inputIcon"
-            >alternate_email</span
-          >
-          <input
-            type="text"
-            class="inputField"
-            id="username"
-            placeholder="Username"
-          />
-        </div>
 
-        <div class="inputContainer">
-          <span class="material-symbols-outlined inputIcon">lock</span>
-          <input
-            type="password"
-            class="inputField"
-            id="password"
-            placeholder="Password"
-          />
-        </div>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" type="text/css" href="form.css" />
+  <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+  <title>Document</title>
+</head>
 
-        <button id="button">Login</button>
-      </form>
-    </div>
-  </body>
+<body>
+  <div class="formcon">
+    <form action="" class="form_main">
+      <p class="heading"><img src="image/coffe.png" /></p>
+      <div class="inputContainer">
+        <span class="material-symbols-outlined inputIcon">alternate_email</span>
+        <input type="text" class="inputField" id="username" placeholder="Username" />
+      </div>
+
+      <div class="inputContainer">
+        <span class="material-symbols-outlined inputIcon">lock</span>
+        <input type="password" class="inputField" id="password" placeholder="Password" />
+      </div>
+
+      <button id="button">Login</button>
+    </form>
+  </div>
+</body>
+
 </html>
